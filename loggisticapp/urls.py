c67e2f0002ed00502b9198567d3985ca52fea2fc
Ali Raza <alirazacammando@gmail.com>
2021-08-23 18:26:56 +0500

    firstde6

--- loggisticapp/urls.py
+++ loggisticapp/urls.py
@@ -2,10 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-
-    url(r'', views.home, name="homepage"),
+    url(r'home', views.home, name="homepage"),
     url(r'dashboard', views.dashboard, name="dashboardpage"),
     url(r'MyShipments', views.MyShipments, name="MyShipmentspage"),
     url(r'MyConsolidations', views.MyConsolidations, name="MyConsolidationspage"),
-
 ]
