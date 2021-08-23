b438b208a0d57604b94dbfb6d4730aa0ecdbbdcc
Ali Raza <alirazacammando@gmail.com>
2021-08-23 17:46:45 +0500

    firstde1

--- loggisticapp/urls.py
+++ loggisticapp/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
 
-    url(r'home', views.home, name="homepage"),
+    url(r'', views.home, name="homepage"),
     url(r'dashboard', views.dashboard, name="dashboardpage"),
     url(r'MyShipments', views.MyShipments, name="MyShipmentspage"),
     url(r'MyConsolidations', views.MyConsolidations, name="MyConsolidationspage"),
